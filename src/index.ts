f831ea44b930824b7a261cf5e7ff61ad9d570e97
Tom Österlund <tsd@spacesquad.de>
2022-01-27 23:26:24 +0100

    feat(): update all exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-
+export * as groupInterface from './interfaces/group.interface'
+export * as locationInterface from './interfaces/location.interface'
+export * as scheduleInterface from './interfaces/schedule.interface'
+export * as scheduleEventInterface from './interfaces/schedule-event.interface'
+export * as schoolInterface from './interfaces/school.interface'
+export * as sessionInterface from './interfaces/session.interface'
+export * as userInterface from './interfaces/user.interface'
 
 export * as types from './types/index'
