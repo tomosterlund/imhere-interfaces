e38b6aaae44a20c5e8452635f17e6c9a29fdcb51
Tom Österlund <tsd@spacesquad.de>
2022-02-07 22:44:28 +0100

    feat: add groups to the school interface

--- src/interfaces/school-interface.ts
+++ src/interfaces/school-interface.ts
@@ -1,8 +1,10 @@
 import {locationInterface} from "./location-interface";
+import {groupInterface} from "./group-interface";
 
 export interface schoolInterface {
 	_id?: string;
 	name: string;
 	ownerId?: string;
 	locations?: locationInterface[];
+	groups?: groupInterface[];
 }
